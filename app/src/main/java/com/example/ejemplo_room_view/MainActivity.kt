680cc5dc653d9ede2199916877c6bd6efa13736a
carmenortuno <carmenortuno@elcampico.org>
2024-01-08 13:09:52 +0100

    cambios_finales

--- app/src/main/java/com/example/ejemplo_room_view/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo_room_view/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.ejemplo_room_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ejemplo_room_view.Adaptadores.TareaListaAdapter
+import com.example.ejemplo_room_view.Utils.TareaAppplication
+import com.example.ejemplo_room_view.ViewModel.TareaViewModel
+import com.example.ejemplo_room_view.ViewModel.TareaViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private val tareaViewModel: TareaViewModel  by viewModels{
+        TareaViewModelFactory((application as TareaAppplication).repositorio)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +26,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter=adaptador
 
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        tareaViewModel.listaTareas.observe(this, Observer { tareas ->
+            tareas?.let { adaptador.submitList(it) }
+        })
     }
 }
